689030760f53f58f7dfa6277a1f4dc32df675427
raineer <delaritarainer81@gmail.com>
2018-02-11 22:30:59 +0800

    home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,19 @@
+import { File} from '@ionic-native/file';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController, ToastController, LoadingController } from 'ionic-angular';
+import {
+  AwsProvider
+} from './../../providers/aws/aws';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  images = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private loadingCtrl: LoadingController, private toastCtrl: ToastController, private awsProvider : AwsProvider, private actionSHeetCtrl: ActionSheetController, private file: File, private camera: Camera) {
 
   }
 
