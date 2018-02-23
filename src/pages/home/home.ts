b66d4659d0a3f9cd0e3399ba7c50536b659396cd
Raineer <delaritaraineer81@gmail.com>
2018-02-23 11:15:16 +0800

    db test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,13 +12,14 @@ import {
 })
 export class HomePage {
   images = [];
+  name: string;
 
   constructor(public navCtrl: NavController, private loadingCtrl: LoadingController, private toastCtrl: ToastController, private awsProvider : AwsProvider, private actionSHeetCtrl: ActionSheetController, private file: File, private camera: Camera) {
 
   }
 
   ionViewWillEnter() {
-    this.loadImages();
+  this.loadImages();
   }
   loadImages() {
       this.images = [];
@@ -34,86 +35,86 @@ export class HomePage {
       });
   }
 
-  presentActionSheet() {
-      let actionSheet = this.actionSHeetCtrl.create({
-        title: 'Select Image Source',
-        buttons: [
-          {
-            text: 'Load from Library',
-            handler:() => {
-              this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
-            }
-          },
-          {
-            text: 'Use Camera',
-            handler: () => {
-              this.takePicture(this.camera.PictureSourceType.CAMERA);
-            }
-          },
-          {
-            text: 'Cancel',
-            role: 'Cncel'
-          }
+  // presentActionSheet() {
+  //     let actionSheet = this.actionSHeetCtrl.create({
+  //       title: 'Select Image Source',
+  //       buttons: [
+  //         {
+  //           text: 'Load from Library',
+  //           handler:() => {
+  //             this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+  //           }
+  //         },
+  //         {
+  //           text: 'Use Camera',
+  //           handler: () => {
+  //             this.takePicture(this.camera.PictureSourceType.CAMERA);
+  //           }
+  //         },
+  //         {
+  //           text: 'Cancel',
+  //           role: 'Cncel'
+  //         }
 
-        ]
-      });
-      actionSheet.present();
-  }
+  //       ]
+  //     });
+  //     actionSheet.present();
+  // }
 
-  takePicture(sourceType) {
-    //Create options for the Camera Dialog
-    const options: CameraOptions = {
-      quality: 100,
-      correctOrientation: true,
-      destinationType: this.camera.DestinationType.FILE_URI,
-      encodingType: this.camera.EncodingType.JPEG,
-      sourceType: sourceType
-    }
+  // takePicture(sourceType) {
+  //   //Create options for the Camera Dialog
+  //   const options: CameraOptions = {
+  //     quality: 100,
+  //     correctOrientation: true,
+  //     destinationType: this.camera.DestinationType.FILE_URI,
+  //     encodingType: this.camera.EncodingType.JPEG,
+  //     sourceType: sourceType
+  //   }
 
-    //Get the picture
-    this.camera.getPicture(options).then((ImageData) => {
-      let loading = this.loadingCtrl.create();
-      loading.present();
+  //   //Get the picture
+  //   this.camera.getPicture(options).then((ImageData) => {
+  //     let loading = this.loadingCtrl.create();
+  //     loading.present();
 
-      //Resolve the picture URI to a file
-      this.file.resolveLocalFilesystemUrl(ImageData).then(oneFile => {
-        //Convert the file to an ArrayBuffer for upload
-        this.file.readAsArrayBuffer(this.file.tempDirectory, oneFile.name).then(realFile => {
-            let type = 'jpg';
-            let newName = this.awsProvider.randomString(6) +new Date().getTime() + '.' + type;
+  //     //Resolve the picture URI to a file
+  //     this.file.resolveLocalFilesystemUrl(ImageData).then(oneFile => {
+  //       //Convert the file to an ArrayBuffer for upload
+  //       this.file.readAsArrayBuffer(this.file.tempDirectory, oneFile.name).then(realFile => {
+  //           let type = 'jpg';
+  //           let newName = this.awsProvider.randomString(6) +new Date().getTime() + '.' + type;
 
-            //Get the URL for our PUT request
-            this.awsProvider.getSignedUploadRequest(newName, 'image/jpg').subscribe(data => {
-              let reqUrl = data.signedRequest;
+  //           //Get the URL for our PUT request
+  //           this.awsProvider.getSignedUploadRequest(newName, 'image/jpg').subscribe(data => {
+  //             let reqUrl = data.signedRequest;
 
-              //Finally upload the file (arrayBuffer) to AWS
-              this.awsProvider.uploadFile(reqUrl, realFile).subscribe(result => {
-                this.awsProvider.getSignedFileRequest(newName).subscribe(res => {
-                  this.images.push({key: newName, url: res});
-                  loading.dismiss();
-                });
-              });
+  //             //Finally upload the file (arrayBuffer) to AWS
+  //             this.awsProvider.uploadFile(reqUrl, realFile).subscribe(result => {
+  //               this.awsProvider.getSignedFileRequest(newName).subscribe(res => {
+  //                 this.images.push({key: newName, url: res});
+  //                 loading.dismiss();
+  //               });
+  //             });
 
-            });
-        }, err => {
-          console.log('err: ', err);
-        })
-      }, (err) => {
-          console.log('err: ', err);
-      });
+  //           });
+  //       }, err => {
+  //         console.log('err: ', err);
+  //       })
+  //     }, (err) => {
+  //         console.log('err: ', err);
+  //     });
 
 
-    })
-  }
+  //   })
+  // }
 
-  deleteImage(index){
-    let toRemove = this.images.splice(index, 1);
-    this.awsProvider.deleteFile(toRemove[0]['key']).subscribe(res => {
-      let toast = this.toastCtrl.create({
-        message: res['msg'],
-        duration: 2000
-      });
-    })
-  }
+  // deleteImage(index){
+  //   let toRemove = this.images.splice(index, 1);
+  //   this.awsProvider.deleteFile(toRemove[0]['key']).subscribe(res => {
+  //     let toast = this.toastCtrl.create({
+  //       message: res['msg'],
+  //       duration: 2000
+  //     });
+  //   })
+  // }
 
 }
--- src/providers/aws/aws.ts
+++ src/providers/aws/aws.ts
@@ -5,37 +5,41 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 @Injectable()
 export class AwsProvider {
-  apiUrl = 'http://localhost:5000/';
+  apiUrl = "http://localhost:8081/";
 
   constructor(public http: Http) {}
-  getSignedUploadRequest(name, type) {
-    return this.http.get(`${this.apiUrl}aws/sign?file-name=${name}&file-type=${type}`).map(res => res.json());
-  }
-  getFileList(): Observable<Array<any>> {
-    return this.http.get(`${this.apiUrl}aws/files`)
-      .map(res => res.json())
-      .map(res => {
-        return res["Contents"].map(val => val.Key);
-      });
-  }
+  // getSignedUploadRequest(name, type) {
+  //   return this.http.get(`${this.apiUrl}aws/sign?file-name=${name}&file-type=${type}`).map(res => res.json());
+  // }
+  // getFileList(): Observable<Array<any>> {
+  //   return this.http.get(`${this.apiUrl}aws/files`)
+  //     .map(res => res.json())
+  //     .map(res => {
+  //       return res["Contents"].map(val => val.Key);
+  //     });
+  // }
   getSignedFileRequest(name) {
     return this.http.get(`${this.apiUrl}aws/files/${name}`).map(res => res.json());
   }
-  deleteFile(name) {
-    return this.http
-      .delete(`${this.apiUrl}aws/files/${name}`)
-      .map(res => res.json());
-  }
-  randomString = function(length) {
-    var text = "";
-    var possible =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-  };
-  uploadFile(url, file) {
-    return this.http.put(url, file);
+  // deleteFile(name) {
+  //   return this.http
+  //     .delete(`${this.apiUrl}aws/files/${name}`)
+  //     .map(res => res.json());
+  // }
+  // randomString = function(length) {
+  //   var text = "";
+  //   var possible =
+  //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  //   for (var i = 0; i < length; i++) {
+  //     text += possible.charAt(Math.floor(Math.random() * possible.length));
+  //   }
+  //   return text;
+  // };
+  // uploadFile(url, file) {
+  //   return this.http.put(url, file);
+  // }
+  
+  getFileList(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}v1/item/`).map(res => res.json());
   }
 }
